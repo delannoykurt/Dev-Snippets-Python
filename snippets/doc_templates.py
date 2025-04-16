e91ccddf6aa92eac59848683bc3cd656f024bab9
delannoykurt <delannoykurt666@gmail.com>
2025-04-16 10:36:36 +0200

    adding one templating for snippets

---
+++ snippets/doc_templates.py
@@ -0,0 +1,10 @@
+def ma_fonction(param1, param2):
+    """
+    Description : Explique ce que fait cette fonction.
+    Paramètres :
+        param1 (type) : Description
+        param2 (type) : Description
+    Retour :
+        type : Description
+    """
+    pass
